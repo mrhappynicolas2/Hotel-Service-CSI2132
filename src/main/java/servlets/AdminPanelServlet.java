420bda68697d7da6936c7afbf6b7bef611d25e2f
mrhappynicolas2 <nberu056@uottawa.ca>
2023-03-31 23:56:09 -0400

    Final stuff
    fixing some of the admin (hotel)

--- src/main/java/servlets/AdminPanelServlet.java
+++ src/main/java/servlets/AdminPanelServlet.java
@@ -23,7 +23,7 @@ public class AdminPanelServlet extends HttpServlet {
         String username = user.split("_")[0];
         String password = user.split("_")[1];
 
-        String hotelName = String.valueOf(session.getAttribute("Hotels"));
+        String hotelName = String.valueOf(request.getParameter("Hotels"));
 
         String url = "jdbc:postgresql://127.0.0.1:5432/postgres?currentSchema=public";
         Application app = new Application(url, username, password);
@@ -58,7 +58,7 @@ public class AdminPanelServlet extends HttpServlet {
         // List customer information with options to insert, delete or update
         String[] select = {"ssnc", "name", "adress", "username", "password", "registrationdate", "phone"};
         String tablename = "customer";
-        String[] where = {"ssnc = " + customerSSN};
+        String[] where = {"ssnc = '" + customerSSN+"'"};
         List<String> queriedCustomers = app.selectFromTable(tablename, select, "Hotels", where);
         List<String> correctCustomer = new ArrayList<>();
         String query = "ssnc: " + customerSSN;
@@ -189,7 +189,7 @@ public class AdminPanelServlet extends HttpServlet {
         // List employee information with options to insert, delete or update
         String[] select = {"ssne", "name", "adress", "username", "password", "hiredate"};
         String tablename = "employee";
-        String[] where = {"ssne = " + employeeSSN};
+        String[] where = {"ssne = '" + employeeSSN+ "'"};
         List<String> queriedEmployees = app.selectFromTable(tablename, select, "Hotels", where);
         List<String> correctEmployees = new ArrayList<>();
         String query = "ssne: " + employeeSSN;
@@ -315,10 +315,10 @@ public class AdminPanelServlet extends HttpServlet {
         // List hotel and room information with options to insert, delete or update
         String[] selectHotel = {"name", "number_rooms", "chain", "stars", "adress", "email", "phone"};
         String tableNameHotel = "hotels";
-        String[] whereHotel = {"name = " + hotelName};
+        String[] whereHotel = {"name = '" + hotelName+"'"};
         List<String> queriedHotels = app.selectFromTable(tableNameHotel, selectHotel, "Hotels", whereHotel);
         List<String> correctHotel = new ArrayList<>();
-        String queryHotels = "\"Hotels\".hotels.name: " + hotelName;
+        String queryHotels = "name: " + hotelName;
 
         for (String s : queriedHotels) {
             if (s.startsWith(queryHotels)) {
@@ -326,13 +326,13 @@ public class AdminPanelServlet extends HttpServlet {
                 correctHotel.add(s);
             }
         }
-
+        
         String[] selectRooms = {"room_num", "room_type", "room_price", "room_capacity", "room_status", "room_annimities", "hotel_name", "agreement_num"};
         String tableNameRooms = "rooms";
-        String[] whereRooms = {"hotel_name = " + hotelName};
+        String[] whereRooms = {"hotel_name = '" + hotelName+"'"};
         List<String> queriedRooms = app.selectFromTable(tableNameRooms, selectRooms, "Hotels", whereRooms);
         List<String> correctRooms = new ArrayList<>();
-        String queryRooms = "\"Hotels\".rooms.hotel_name: " + hotelName;
+        String queryRooms = "hotel_name: " + hotelName;
 
         for (String s : queriedRooms) {
             if (s.startsWith(queryRooms)) {
@@ -340,6 +340,7 @@ public class AdminPanelServlet extends HttpServlet {
                 correctRooms.add(s);
             }
         }
+        
 
 
         String beginHotel = "<!DOCTYPE html>\n" +
@@ -384,7 +385,7 @@ public class AdminPanelServlet extends HttpServlet {
         for (int i = 0; i < correctHotel.size(); i++) {
             String[] hotelInfo = correctHotel.get(i).split(",");
             String[] fixedHotelInfo = new String[7];
-            for (int j = 0; j < hotelInfo.length; j++) {
+            for (int j = 0; j < hotelInfo.length-1; j++) {
                 String[] tempInfo = hotelInfo[j].split(":");
                 fixedHotelInfo[j] = tempInfo[1];
             }
@@ -456,10 +457,10 @@ public class AdminPanelServlet extends HttpServlet {
     public void roomMenu(Application app, PrintWriter out, String hotelName, String roomnum, HttpSession session){
         String[] selectRooms = {"room_num", "room_type", "room_price", "room_capacity", "room_status", "room_annimities", "hotel_name", "agreement_num"};
         String tableNameRooms = "rooms";
-        String[] whereRooms = {"hotel_name = " + hotelName};
+        String[] whereRooms = {"hotel_name = '" + hotelName+"'"};
         List<String> queriedRooms = app.selectFromTable(tableNameRooms, selectRooms, "Hotels", whereRooms);
         List<String> correctRooms = new ArrayList<>();
-        String queryRooms = "\"Hotels\".rooms.hotel_name: " + hotelName;
+        String queryRooms = "hotel_name: " + hotelName;
 
         for (String s : queriedRooms) {
             if (s.startsWith(queryRooms)) {
